7735a1b18a2dce6c44f5fb985046455e0e63cf2d
Sydney C Doemel Doemel <sydneycdoemeldoemel@Sydneys-MacBook-Pro.local>
2022-07-29 13:44:12 -0400

    idk

--- app/models.py
+++ app/models.py
@@ -8,11 +8,7 @@ class User(db.Model):
     username = db.Column(db.String(50), nullable=False, unique=True)
     email = db.Column(db.String(150), nullable=False, unique=True)
     password = db.Column(db.String(200), nullable=False)
-    poke1 = db.Column(db.String(200))
-    poke2 = db.Column(db.String(200))
-    poke3 = db.Column(db.String(200))
-    poke4 = db.Column(db.String(200))
-    poke5 = db.Column(db.String(200))
+   
 
     def __init__(self, username, email, password):
         self.username = username
